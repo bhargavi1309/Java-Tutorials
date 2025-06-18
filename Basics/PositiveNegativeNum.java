f4b171116b3a4f0997c72342cc82e6bf8a180c2c
bhargavi1309 <168528983+bhargavi1309@users.noreply.github.com>
2025-06-18 22:33:56 +0530

    Add files via upload

---
+++ Basics/PositiveNegativeNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PositiveNegativeNum
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num>0)
+		{
+			System.out.println("It is a positive number");
+		}
+		else
+		{
+			System.out.println("It is a negative number");
+		}
+	}
+}
