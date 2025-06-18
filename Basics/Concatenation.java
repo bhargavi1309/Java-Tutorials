ce08429fc93b2c290240073d24c8669850b9750e
bhargavi1309 <168528983+bhargavi1309@users.noreply.github.com>
2025-06-18 22:30:42 +0530

    Add files via upload

---
+++ Basics/Concatenation.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class Concatenation
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		String result=""+a+b;
+		System.out.println(result);
+	}
+}
+		
